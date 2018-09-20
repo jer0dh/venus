38d196eb1e1e4502dd7d449733eb37a519cd107a
jer0dh <jerod@jhtechservices.com>
2018-09-20 12:18:00 -0500

    moving all to front-page.php, adding smooth-scroll and custom js, adding code to alter menu urls on front page to bookmark if url is in the multipage menu

--- theme_src/libs/multipage.php
+++ theme_src/libs/multipage.php
@@ -1,6 +1,13 @@
 <?php
 
+//TODO add edit link to each page - edit_post_link()
 
+remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+add_action( 'genesis_entry_content', 'venus_do_page_content' );
+
+function venus_do_page_content() {
+	the_content();
+}
 function venus_multipage_custom_loop() {
 
 	global $wp_query;
@@ -18,4 +25,37 @@ function venus_multipage_custom_loop() {
 	genesis_standard_loop();
 
 	wp_reset_query();
+}
+
+add_filter('wp_nav_menu_objects', 'venus_test_menu', 10, 2);
+global $venus_test;
+function venus_test_menu( $menus, $args ) {
+	global $venus_test;
+
+	if ( isset( $args ) ) {
+		if ( isset( $args->theme_location ) || $args->theme_location !== '' ) {
+
+			$multipages_menu = wp_get_nav_menu_items( 'multipage' );
+			$multipages_urls = array_map( function ( $menu ) {
+				return $menu->url;
+			}, $multipages_menu );
+
+
+			foreach ( $menus as &$menu ) {
+				if ( in_array( $menu->url, $multipages_urls ) ) {
+					$page = get_post( $menu->object_id );
+					if ( $page !== null ) {
+						if ( is_front_page() ) {
+							$menu->url = '#' . sanitize_title_with_dashes( $page->post_title );
+/*						} else {
+							$menu->url = home_url('#' . sanitize_title_with_dashes( $page->post_title )); */
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	return $menus;
 }
--- theme_src/multipage-template.php
+++ theme_src/multipage-template.php
@@ -2,51 +2,12 @@
 /*
  * Template Name: Multipage
  */
-/**
- * Thinking that this will cause google to think we have many pages with the same information.  Maybe we use a redirect
- * so this will always be the home page.  Use the following to pass in a GET variable so we know what page the user was
- * trying for and scroll to that section:
- *
-wp_redirect( esc_url( add_query_arg( 'variable_to_send', '1', home_url() ) ) );
+
+//include get_stylesheet_directory() . '/front-page.php';
+
+$var = get_queried_object();
+
+wp_redirect( esc_url( home_url() . '#' . sanitize_title_with_dashes($var->post_title) ) );
 exit;
-}
- *
- * Then Put all the following code in the front-page.php
- *
- */
-/**
- * Or maybe skip all this and just use ACF and sections on the front page
- */
 
-remove_action( 'genesis_loop', 'genesis_do_loop' );
-add_action( 'genesis_loop', 'venus_multipage_custom_loop' );
-add_action( 'genesis_before_loop', 'venus_add_page_info' );
-add_action('wp_head', 'venus_add_multipage_js');
-
-
-function venus_add_multipage_js() {
-
-	wp_enqueue_script( 'multipage', get_stylesheet_directory_uri() . '/js/multipage.js', array('jquery'),null, true);
-}
-
-function venus_add_page_info() {
-
-	$var = get_queried_object();
-
-	$id = sanitize_title_with_dashes( $var->post_title );
-
-	wp_localize_script( 'multipage', 'wp_multipage', array( 'id' => $id ) );
-
-}
-
-
-add_filter( 'genesis_attr_entry', 'custom_attributes_content' );
-
-function custom_attributes_content( $attributes ) {
-
-	$attributes['id'] = sanitize_title_with_dashes( get_the_title() );
-
-	return $attributes;
-}
-
-genesis();
+//add_query_arg( 'location', sanitize_title_with_dashes($var->post_title), home_url() )
