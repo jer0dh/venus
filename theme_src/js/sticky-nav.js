00071aa1c7e6805604043207a668214e9693e811
jer0dh <jerod@jhtechservices.com>
2018-09-21 07:32:48 -0500

    sticky nav work

---
+++ theme_src/js/sticky-nav.js
@@ -0,0 +1,23 @@
+//requires debounce.js
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const nav = document.querySelector('.site-header');
+    const navTop = nav.offsetTop;
+  //  const originalBodyPadding = document.body.style.paddingTop;
+
+    const stickyNav = new Debouncer( function() {
+        if(window.scrollY >= navTop) {
+            document.body.style.paddingTop = nav.offsetHeight + 'px';
+            document.body.classList.add('fixed-nav');
+        } else {
+            document.body.classList.remove('fixed-nav');
+       //     document.body.style.paddingTop = originalBodyPadding;
+            document.body.style.paddingTop = 0;
+        }
+    });
+
+    window.addEventListener('scroll', stickyNav.handleEvent.bind(stickyNav));
+
+});
