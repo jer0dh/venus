96501a118ef3b1c2ce6453a13d3bdfd22c7ceea2
jer0dh <jerod@jhtechservices.com>
2018-10-17 18:58:14 -0500

    breadcrumbs, archive page, single page, consolidating multipage code

--- theme_src/front-page.php
+++ theme_src/front-page.php
@@ -1,29 +1,10 @@
 <?php
 
 
-//* add wrap around page titles (entry-header)
-add_action( 'genesis_entry_header', 'venus_add_wrap_open', 6 );
-add_action( 'genesis_entry_header', 'venus_add_wrap_close', 14 );
-
-/* We want full page content, not excerpt */
-remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
-add_action( 'genesis_entry_content', 'venus_do_page_content' );
-
 
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', 'venus_multipage_custom_loop' );
 
 
-/**
- * Add the id to the article tag
- */
-
-add_filter( 'genesis_attr_entry', 'custom_attributes_content' );
-function custom_attributes_content( $attributes ) {
-
-	$attributes['id'] = sanitize_title_with_dashes( get_the_title() );
-
-	return $attributes;
-}
 
 genesis();
--- theme_src/libs/helpers.php
+++ theme_src/libs/helpers.php
@@ -19,12 +19,73 @@ function venus_do_page_content() {
 
 function venus_add_wrap_open() {
 	?>
-	<div class="wrap">
+    <div class="wrap">
 	<?php
 }
 
 function venus_add_wrap_close() {
 	?>
-	</div><!-- wrap -->
+    </div><!-- wrap -->
 	<?php
+}
+
+//Remove default 'You are here' prefix and 'Archives for'
+add_filter( 'genesis_breadcrumb_args', 'venus_breadcrumbs_args' );
+function venus_breadcrumbs_args( $args ) {
+
+
+	$args['labels']['prefix']   = '';
+	$args['labels']['category'] = '';
+
+	return $args;
+}
+
+// Change category crumbs to always be the default category link
+add_filter( 'genesis_post_crumb', 'venus_post_crumb' );
+function venus_post_crumb( $crumb ) {
+    $archive_category = get_field( 'archive_category', 'options');
+    if($archive_category) {
+        $term = get_term( $archive_category );
+        if(!is_wp_error($term)) {
+            $crumb = sprintf('<a href="%s">%s</a>', esc_url(get_category_link($term->term_id)),esc_textarea( $term->name));
+        }
+    }
+    return $crumb;
+}
+
+/**
+ * prints out a list of categories the post belongs to
+ */
+function venus_add_categories() {
+	$cats = get_the_category();
+	?>
+    <ul class="post-categories">
+		<?php foreach ( $cats as $cat ) : ?>
+            <li class="btn btn-info"><?php echo $cat->name; ?></li>
+
+		<?php endforeach; ?>
+    </ul>
+	<?php
+}
+
+
+function venus_remove_all_post_actions() {
+	remove_action( 'genesis_entry_header', 'genesis_do_post_format_image', 4 );
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+	remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+	remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+
+	remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+	remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+	remove_action( 'genesis_entry_content', 'genesis_do_post_content_nav', 12 );
+	remove_action( 'genesis_entry_content', 'genesis_do_post_permalink', 14 );
+
+	remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+	remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+	remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+
+	remove_action( 'genesis_after_entry', 'genesis_do_author_box_single', 8 );
+	remove_action( 'genesis_after_entry', 'genesis_adjacent_entry_nav' );
+	remove_action( 'genesis_after_entry', 'genesis_get_comments_template' );
 }
