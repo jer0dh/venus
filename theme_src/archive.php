7b8e319380bc740d064eea74e0aaf9558c9847f5
jer0dh <jerod@jhtechservices.com>
2019-03-16 21:19:31 -0500

    making title linkable on archive page

--- theme_src/archive.php
+++ theme_src/archive.php
@@ -18,7 +18,7 @@ venus_remove_all_post_actions();
 add_action( 'genesis_entry_header', 'venus_do_post_image', 2 );
 add_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
 add_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
-add_action( 'genesis_entry_header', 'genesis_do_post_title' );
+add_action( 'genesis_entry_header', 'venus_do_post_title' );
 add_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
 add_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
 
@@ -46,6 +46,57 @@ add_filter( 'genesis_attr_entry', 'venus_add_article_class');
 function venus_add_article_class($att) {
     $att['class'] .= ' col-md-3';
     return $att;
+}
+
+function venus_do_post_title() {
+
+	$title = apply_filters( 'genesis_post_title_text', get_the_title() );
+
+	if ( '' === trim( $title ) ) {
+		return;
+	}
+
+	// Link it, if necessary.
+
+		$title = genesis_markup( array(
+			'open'    => '<a %s>',
+			'close'   => '</a>',
+			'content' => $title,
+			'context' => 'entry-title-link',
+			'echo'    => false,
+		) );
+
+	// Wrap in H1 on singular pages.
+	$wrap = is_singular() ? 'h1' : 'h2';
+
+	// Also, if HTML5 with semantic headings, wrap in H1.
+	$wrap = genesis_html5() && genesis_get_seo_option( 'semantic_headings' ) ? 'h1' : $wrap;
+
+	/**
+	 * Entry title wrapping element.
+	 *
+	 * The wrapping element for the entry title.
+	 *
+	 * @since 2.2.3
+	 *
+	 * @param string $wrap The wrapping element (h1, h2, p, etc.).
+	 */
+	$wrap = apply_filters( 'genesis_entry_title_wrap', $wrap );
+
+	// Build the output.
+	$output = genesis_markup( array(
+		'open'    => "<{$wrap} %s>",
+		'close'   => "</{$wrap}>",
+		'content' => $title,
+		'context' => 'entry-title',
+		'params'  => array(
+			'wrap' => $wrap,
+		),
+		'echo'    => false,
+	) );
+
+	echo apply_filters( 'genesis_post_title_output', $output, $wrap, $title ) . "\n"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
 }
 
 genesis();
